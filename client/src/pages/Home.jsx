5161e3df3b1a20a3462fea2ccb3b17e98c5ac609
Tamerbekir <astoryofame@gmail.com>
2024-05-20 21:53:10 -0400

    Added pages for react and some pseudo coding for each page. Added comment.js and comment.jsx in comps for comment section

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,5 @@
+// List of categories to pick from
+    // clicking on category takes user to the tutorial:id
+// Logout, login, sign up
+
+// User can view categories but they cannot view individual tutorials
---
+++ client/src/pages/Tutorial.jsx
@@ -0,0 +1,5 @@
+// Tutorial user clicked on from the home.jsx
+// This is where a single tutorial will be rendered
+// Will be tied to model tutorial ID
+// Comment on tutorial
+    // Able to delete comment by user who made comment. 
