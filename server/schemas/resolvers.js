dd06fc861581c5d6141044c0af2fe4a680ddd448
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-05-21 22:07:59 -0400

    commit

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,7 @@
-<<<<<<< HEAD
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { AuthenticationError } = require('apollo-server-express');
+
 const { User, Category, Tutorial, Comment } = require('../models');
 
 const resolvers = {
@@ -59,23 +62,3 @@ const resolvers = {
 module.exports = resolvers;
 
 User, Category, Tutorial, Comment
-=======
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { AuthenticationError } = require('apollo-server-express');
-
-// Bringing in models 
-const Profile = require('../models/Profile')
-const Tutorial = require('../models/Tutorial')
-const Comment = require('../models/Comment')
-const Category = require('../models/Category')
-
-const resolvers = {
-  Query: {
-    // Get all profiles and populate all comments and tutorials
-    profile: async (parent, args, { _id }) => {
-      return Profile.findById(_id).populate('tutorials').populate('comments');
-    },
-  }
-}
->>>>>>> 9b7e4773b51bd244f7683773785ee1a9a08378b5
