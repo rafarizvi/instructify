fc0cdcc744818b5b44a37c93b23569d2bdb88c39
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-05-22 19:23:37 -0400

    commit to pull

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['axios']
+    server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/graphql': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+    build: {
+      rollupOptions: {
+        external: ['axios']
+      }
     }
   }
 })
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,13 +2,13 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { AuthenticationError } = require('apollo-server-express');
 
-const { User, Category, Tutorial, Comment } = require('../models');
+const { Profile, Category, Tutorial, Comment } = require('../models');
 
 const resolvers = {
   Query: {
-    user: async (_, { _id }) => {
+    profile: async (_, { _id }) => {
       const params = _id ? { _id } : {};
-      return User.findById(params);
+      return Profile.findById(params);
     },
 
     tutorials: async () => {
@@ -31,34 +31,20 @@ const resolvers = {
     comments(parent) {
       return Comment.findById(parent._id);
     }
+  },
+
+  Tutorial: {
+    comments(parent) {
+      return Comment.findById(parent._id);
+    }
+  },
+
+  Category: {
+    tutorials(parent) {
+      return Tutorial.findById(parent._id);
+    }
   }
-
-  // Tutorial: {
-  //   author: (_, args, context, info) => {
-  //     // Resolve the author of the tutorial
-
-  //   },
-  //   category: (_, args, context, info) => {
-  //     // Resolve the category of the tutorial
-  //   },
-  //   comments: (_, args, context, info) => {
-  //     // Resolve the comments of the tutorial
-  //   }
-
-  // },
-
-  // Comment: {
-  //   author: (_, args, context, info) => {
-  //     // Resolve the author of the comment
-  //   },
-  //   tutorial: (_, args, context, info) => {
-  //     // Resolve the tutorial of the comment
-  //   }
-  // }
 
 }
 
-
 module.exports = resolvers;
-
-User, Category, Tutorial, Comment
