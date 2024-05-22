15bc2d8e07dfa6c6fb7a2e158c2edefeec08e053
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-05-21 22:02:07 -0400

    commit to pull

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -20,48 +20,40 @@ const resolvers = {
     }
   },
 
-  Tutorial: {
-    author: (_, args, context, info) => {
-      // Resolve the author of the tutorial
+  Profile: {
+    tutorials(parent) {
+      return Tutorial.findById(parent._id);
     },
-    category: (_, args, context, info) => {
-      // Resolve the category of the tutorial
-    },
-    comments: (_, args, context, info) => {
-      // Resolve the comments of the tutorial
-    }
-
-  },
-
-  Comment: {
-    author: (_, args, context, info) => {
-      // Resolve the author of the comment
-    },
-    tutorial: (_, args, context, info) => {
-      // Resolve the tutorial of the comment
+    comments(parent) {
+      return Comment.findById(parent._id);
     }
   }
-};
 
+  // Tutorial: {
+  //   author: (_, args, context, info) => {
+  //     // Resolve the author of the tutorial
 
+  //   },
+  //   category: (_, args, context, info) => {
+  //     // Resolve the category of the tutorial
+  //   },
+  //   comments: (_, args, context, info) => {
+  //     // Resolve the comments of the tutorial
+  //   }
 
+  // },
 
+  // Comment: {
+  //   author: (_, args, context, info) => {
+  //     // Resolve the author of the comment
+  //   },
+  //   tutorial: (_, args, context, info) => {
+  //     // Resolve the tutorial of the comment
+  //   }
+  // }
 
-Mutation: {
-  createMatchup: async (_, args) => {
-    const matchup = await Matchup.create(args);
-    return matchup;
-  },
-    createVote: async (_, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
-    },
-  },
-};
+}
+
 
 module.exports = resolvers;
 
