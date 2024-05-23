f91957c72941bfbd321ec29543591fc2ed2d00e1
danielhe27 <jdah49d@gmail.com>
2024-05-23 17:05:35 -0400

    fixing links to login and signup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,7 @@ import { Outlet } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import VideoSearch from './components/videoSearch';
-import Login from './components/Signup';
-import Signup from './components/Login';
+
 
 
 const client = new ApolloClient({
@@ -18,19 +15,10 @@ const client = new ApolloClient({
 
 function App() {
   return (
-    <React.Fragment>
-      <ApolloProvider client={client}>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/search" element={<VideoSearch />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-
-        </Routes>
-        <Outlet />
-      </ApolloProvider>
-    </React.Fragment>
+    <ApolloProvider client={client}>
+      <Navbar />
+      <Outlet />
+    </ApolloProvider>
   );
 }
 
