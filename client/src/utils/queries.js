cd6540eb85bd14697962828bc2ec159bbb2c1b47
danielhe27 <jdah49d@gmail.com>
2024-05-24 16:44:18 -0400

    queries client

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,133 @@
 import { gql } from '@apollo/client';
 
+export const GET_PROFILE = gql`
+  query getProfile($profileId: ID!) {
+    profile(_id: $profileId) {
+      _id
+      name
+      email
+      tutorials {
+        _id
+        title
+        content
+        author {
+          _id
+          name
+          email
+        }
+        category {
+          _id
+          name
+        }
+        comments {
+          _id
+          content
+          author {
+            _id
+            name
+            email
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_TUTORIALS = gql`
+  query getTutorials {
+    tutorials {
+      _id
+      title
+      content
+      author {
+        _id
+        name
+        email
+      }
+      category {
+        _id
+        name
+      }
+      comments {
+        _id
+        content
+        author {
+          _id
+          name
+          email
+        }
+      }
+    }
+  }
+`;
+
+export const GET_CATEGORIES = gql`
+  query getCategories {
+    categories {
+      _id
+      name
+      tutorials {
+        _id
+        title
+        content
+        author {
+          _id
+          name
+          email
+        }
+        category {
+          _id
+          name
+        }
+        comments {
+          _id
+          content
+          author {
+            _id
+            name
+            email
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const GET_COMMENTS = gql`
+  query getComments {
+    comments {
+      _id
+      content
+      author {
+        _id
+        name
+        email
+      }
+      tutorial {
+        _id
+        title
+        content
+        author {
+          _id
+          name
+          email
+        }
+        category {
+          _id
+          name
+        }
+        comments {
+          _id
+          content
+          author {
+            _id
+            name
+            email
+          }
+        }
+      }
+    }
+  }
+`;
+
+
