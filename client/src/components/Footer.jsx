ea138f37ff22a8778cf5c1d5e61ef8330cc783cd
Tamerbekir <astoryofame@gmail.com>
2024-05-24 15:07:24 -0400

    added additional syntax to app.jsx and removed uneeded code. Removed unnecessary code from footer and homepage

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,20 +1,9 @@
-import { useLocation, useNavigate } from 'react-router-dom';
 
 const Footer = () => {
-  const location = useLocation();
-  const navigate = useNavigate();
   return (
-    <footer className="w-100 mt-auto text-dark p-4">
-      <div className="container text-center mb-5">
-        {location.pathname !== '/' && (
-          <button
-            className="btn btn-dark mb-3"
-            onClick={() => navigate(-1)}
-          >
-            &larr; Go Back
-          </button>
-        )}
-        <h4>&copy; {new Date().getFullYear()} - Tech Friends</h4>
+    <footer className="text center">
+      <div>
+        <p>A very boring footer for now</p>
       </div>
     </footer>
   );
