f64592c1d0d264d536564e3c2002e9b04907db4e
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-05-26 10:10:11 -0400

    added categories to home page and added some css

--- client/src/components/categories/index.jsx
+++ client/src/components/categories/index.jsx
@@ -1,27 +1,32 @@
 import { Link } from 'react-router-dom';
 
+import './Categories.css';
+
 const CategoryList = ({ categories, title, all }) => {
 
   return (
     <div>
-      <h3 className="text-info text-center">{title}</h3>
-      <div className="flex-row justify-space-between my-4">
+      <div className="col-12">
+        <h1 className="mb-4 text-center">{title}</h1>
+        <p className="lead text-center">Empowering Knowledge Sharing,<br /> One Tutorial at a Time</p>
+      </div>
 
-        <div className="col-12 col-xl-6">
-          <Link className="btn btn-block btn-squared btn-light text-dark category-link ">
-            <h4 className="card-header bg-dark text-light p-2 m-0">{all}</h4>
-          </Link>
-        </div>
+      <div className="catDiv">
+        <Link className="btn btn-squared text-dark category-link"
+          to={'/all'}>
+          <h4 className="bg-dark text-light p-2 m-0 catBtn">{all}</h4>
+        </Link>
+      </div>
 
-
+      <div className='catDiv'>
         {categories &&
           categories.map((category) => (
-            <div key={category._id} className="col-12 col-xl-6">
+            <div key={category._id}>
               <Link
-                className="btn btn-block btn-squared btn-light text-dark category-link"
-                to={`/tutorial/${category.name.toLowerCase()}`}
+                className="btn btn-squared category-link"
+                to={`/${category.name.toLowerCase()}`}
               >
-                <h4 className="card-header bg-dark text-light p-2 m-0">
+                <h4 className="bg-dark text-light p-2 m-0 catBtn">
                   {category.name} <br />
                 </h4>
               </Link>
@@ -29,6 +34,9 @@ const CategoryList = ({ categories, title, all }) => {
           ))}
       </div>
     </div>
+
   );
 };
 export default CategoryList;
+
+
