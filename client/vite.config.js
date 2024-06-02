f7d427568af46d791243aee59d885465144318ab
Tamerbekir <astoryofame@gmail.com>
2024-06-02 15:27:57 -0400

    attempting to get project to work on Render

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     port: 3000,
     open: true,
@@ -16,9 +17,4 @@ export default defineConfig({
       },
     },
   },
-  build: {
-    rollupOptions: {
-      external: ['axios']
-    }
-  }
 })
