b419989732612e9fda81f7779e9e8523f6f06ae0
Tamerbekir <astoryofame@gmail.com>
2024-06-02 15:13:25 -0400

    Added adjustment to package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
+    server: {
     port: 3000,
     open: true,
     proxy: {
@@ -15,10 +15,10 @@ export default defineConfig({
         secure: false,
       },
     },
-  },
-  build: {
-    rollupOptions: {
-      external: ['axios']
+    build: {
+      rollupOptions: {
+        external: ['axios']
+      }
     }
   }
 })
