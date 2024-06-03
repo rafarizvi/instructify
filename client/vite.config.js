2e8d02f65eeb90edb79895dfe01c661ea727d69e
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-06-02 21:59:42 -0400

    changed vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,14 +10,39 @@ export default defineConfig({
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
-        changeOrigin: true,
         secure: false,
-      },
-    },
-  },
-  build: {
-    rollupOptions: {
-      // external: ['axios'] // Remove or comment out this line
+        changeOrigin: true
+      }
     }
+  }, 
+  test: {
+    environment: 'happy-dom',
+    globals: true
   }
 })
+
+
+
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: process.env.PORT || 3000,
+//     open: true,
+//     proxy: {
+//       '/graphql': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//         secure: false,
+//       },
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       // external: ['axios'] // Remove or comment out this line
+//     }
+//   }
+// })
