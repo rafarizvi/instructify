1fb25480daf89d196c0c948f05f64ce7d1787e51
Tamerbekir <astoryofame@gmail.com>
2024-06-02 20:25:16 -0400

    Added to the server file to ensure it connects to mongoDb. It now connects locally as it would if deployed using .env variables.

--- server/server.js
+++ server/server.js
@@ -4,10 +4,11 @@ const { expressMiddleware } = require('@apollo/server/express4');
 const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 const { google } = require('googleapis'); 
-
-
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
+
+const dotenv = require('dotenv');
+dotenv.config();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -17,21 +18,16 @@ const server = new ApolloServer({
   context: authMiddleware
 });
 
-
-// Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
   await server.start();
-
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
-  
-    app.use('/graphql', expressMiddleware(server, {
+  app.use('/graphql', expressMiddleware(server, {
     context: authMiddleware
   }));
 
   if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/dist')));
-
     app.get('*', (req, res) => {
       res.sendFile(path.join(__dirname, '../client/dist/index.html'));
     });
@@ -45,41 +41,4 @@ const startApolloServer = async () => {
   });
 };
 
-// Call the async function to start the server
-  startApolloServer();
-
-
-  // Set up YouTube API client
-  // const youtube = google.youtube({
-  //   version: 'v3',
-  //   auth: process.env.YOUTUBE_API_KEY // Make sure you have this key in your .env file      /* !!watch!!*/
-  // });
-
-  // // Endpoint to search for YouTube videos
-  // app.get('/api/search', async (req, res) => {
-  //   const { query } = req.query;
-  //   try {
-  //     const response = await youtube.search.list({
-  //       part: 'snippet',
-  //       q: query,
-  //       maxResults: 10,
-  //       type: 'video'
-  //     });
-
-  //     const videos = response.data.items.map(item => ({
-  //       title: item.snippet.title,
-  //       description: item.snippet.description,
-  //       thumbnail: item.snippet.thumbnails.default.url,
-  //       videoId: item.id.videoId
-  //     }));
-
-  //     res.json(videos);
-  //   } catch (error) {
-  //     res.status(500).json({ error: error.message });
-  //   }
-  // });
-  
-  // Important for MERN Setup: When our application runs from production, it functions slightly differently than in development
-  // In development, we run two servers concurrently that work together
-  // In production, our Node server runs and delivers our client-side bundle from the dist/ folder 
-
+startApolloServer();
