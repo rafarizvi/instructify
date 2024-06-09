c1e5f5a5c104c2ad3f87aa5ecd4a1004f8458c1a
Tamerbekir <astoryofame@gmail.com>
2024-06-09 14:35:00 -0400

    fixing render issue with names

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,7 @@
 // exporting the components into the main.jsx
-export { default as VideoSearch } from './videoSearch/videoSearch';
+export { default as VideoSearch } from './videoSearch/videoSearch'
 export { default as Login } from './login/Login';
-export { default as Signup } from './signup/Signup';
+export { default as Signup } from './signup/Signup'
 export { default as CreateTutorial } from './createTutorial/CreateTutorial'
 export { default as About } from './about/About'
 export { default as Footer } from './footer/Footer'
